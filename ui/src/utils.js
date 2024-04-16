bc1eb77ad74717e2908a56732144ef5f17621928
AbhishekRai795 <149105339+AbhishekRai795@users.noreply.github.com>
2024-04-17 04:23:21 +0530

    Update utils.js

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://5egl23ronhmzezqptzfyumvlgu0daduu.lambda-url.ap-south-1.on.aws/task";
+export const API_URL = "https://35tmzmmmgba6tornkzz7tea4oi0eyzaf.lambda-url.ap-south-1.on.aws/task";
